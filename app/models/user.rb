c7ed79e2075540f2b2a7ca1c6e484a2c19ccc538
Michael Frister <michael+git@frister.net>
2013-06-13 11:49:58 +0200

    Add force_password_reset field to user edit form
    Also, reindent the form HTML.

--- app/models/user.rb
+++ app/models/user.rb
@@ -540,7 +540,7 @@ class User < Principal
   safe_attributes 'firstname', 'lastname', 'mail', 'mail_notification', 'language',
                   'custom_field_values', 'custom_fields', 'identity_url'
 
-  safe_attributes 'status', 'auth_source_id',
+  safe_attributes 'auth_source_id', 'force_password_reset', 'status',
     :if => lambda {|user, current_user| current_user.admin?}
 
   safe_attributes 'group_ids',
